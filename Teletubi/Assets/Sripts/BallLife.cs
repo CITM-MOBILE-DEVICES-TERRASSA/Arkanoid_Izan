3c12e68b36b33814f8d4f6eb26e337fbb6925f60
izansl <125253405+izansl@users.noreply.github.com>
2024-10-21 09:36:07 +0200

    Pantallitas

--- Teletubi/Assets/Sripts/BallLife.cs
+++ Teletubi/Assets/Sripts/BallLife.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class BallLife : MonoBehaviour
 {
@@ -34,6 +35,7 @@ public class BallLife : MonoBehaviour
 
     void DestroyBall()
     {
-        Destroy(gameObject); 
+        Destroy(gameObject);
+        SceneManager.LoadScene("Game Over");
     }
 }
